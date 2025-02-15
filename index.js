9e43f1683a66a0e6bdc7b1df87b3b6f2df0bcb97
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2025-02-15 22:13:50 +0900

    ESLint 9 migration

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-/** @type {import('eslint/lib/shared/types').ConfigData} */
-module.exports = {
-  plugins: ["import"],
-  rules: {
-    "import/no-default-export": "error",
-    "import/prefer-default-export": "off",
-  },
-  overrides: [
-    {
-      /**
-       * @see https://nextjs.org/docs/app/building-your-application/routing#file-conventions
-       */
-      files: [
-        "{,src/}app/**/layout.{js,jsx,tsx}",
-        "{,src/}app/**/page.{js,jsx,tsx}",
-        "{,src/}app/**/loading.{js,jsx,tsx}",
-        "{,src/}app/**/not-found.{js,jsx,tsx}",
-        "{,src/}app/**/error.{js,jsx,tsx}",
-        "{,src/}app/**/global-error.{js,jsx,tsx}",
-        "{,src/}app/**/template.{js,jsx,tsx}",
-        "{,src/}app/**/default.{js,jsx,tsx}",
-      ],
-      rules: {
-        "import/no-default-export": "off",
-        "import/prefer-default-export": ["error", { target: "any" }],
-      },
+// @ts-check
+
+/** @type { import("eslint").Linter.Config [] } */
+module.exports = [
+  {
+    plugins: {
+      importPlugin: require("eslint-plugin-import"),
     },
-  ],
-};
+  },
+  {
+    files: ["{,src/}app/**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "import/no-default-export": "error",
+      "import/prefer-default-export": "off",
+    },
+  },
+  {
+    /**
+     * @see https://nextjs.org/docs/app/building-your-application/routing#file-conventions
+     */
+    files: [
+      "{,src/}app/**/layout.{js,jsx,tsx}",
+      "{,src/}app/**/page.{js,jsx,tsx}",
+      "{,src/}app/**/loading.{js,jsx,tsx}",
+      "{,src/}app/**/not-found.{js,jsx,tsx}",
+      "{,src/}app/**/error.{js,jsx,tsx}",
+      "{,src/}app/**/global-error.{js,jsx,tsx}",
+      "{,src/}app/**/template.{js,jsx,tsx}",
+      "{,src/}app/**/default.{js,jsx,tsx}",
+    ],
+    rules: {
+      "import/no-default-export": "off",
+      "import/prefer-default-export": ["error", { target: "any" }],
+    },
+  },
+];
